417d7c80132f014b4df037bb370111d1d0f32d27
saadz-khan <saad.zk.2001@gmail.com>
2021-06-10 06:51:55 +0500

    First major additions and milestone added voting system. Some debugging required

--- Project.C
+++ Project.C
@@ -21,6 +21,17 @@ sbit rs = P3 ^ 5;					// 0 : Insturction code input , 1 : Data input
 sbit rw = P3 ^ 6;					// Data Write Pin for LCD Display
 sbit en = P3 ^ 7;					// Enable Signal pin for LCD Display
 
+// Vote variables
+party1 = 0;
+party2 = 0;
+party3 = 0;
+party4 = 0;
+
+unsigned char pin[] = {"12345"};
+unsigned char Epin[9];
+unsigned char vote[] = {"1234"};
+char vote_no[4];
+
 // All functions declarations
 void lcdcmd(unsigned char);
 void lcddat(unsigned char);
@@ -29,9 +40,9 @@ char keypad();
 int check();
 void delay(unsigned int);
 void line_disp(unsigned char *);
-unsigned char pin[] = {"12345"};
-unsigned char Epin[5];
-unsigned char vote[] = {'1','2'};
+void count();
+void thank_func();
+void display_vote(unsigned int party);
 
 // main function
 int main()
@@ -42,10 +53,14 @@ int main()
 
     while (1)
     {
-        unsigned int i = 0;
+        unsigned int i = 0, j = 0;      // Temporary Variable Declaration
+        if (j == 1){
+            break;
+        }
+        lcdcmd(0x01); // Clear screen
         lcdcmd(0x80); //decimal value: 128
         lcddisplay("ENTER PIN NUMBER");
-        delay(1000);	
+        delay(500);	
         lcdcmd(0xc0); //decimal value: 192
         while (pin[i] != '\0')
         {
@@ -56,29 +71,122 @@ int main()
         check();
 		if (check() == 1)
 		{
-		unsigned int m = 0;
-		delay(1000);	
-		line_disp("Vote1 or Bank2");
-        lcdcmd(0xc0); //decimal value: 192
-		while (vote[m] != '\0')
-        {
-            Epin[m] = keypad();
+            lcdcmd(0x80);
+            lcddisplay("Welcome User!");
+            delay(500);
+            lcdcmd(0x80);
+            line_disp("PTI PPP PML JUI");
+            count();
             delay(1000);
-		if (vote[1] == Epin[1])
-	    	{
-		    line_disp("Can 1,Can 2,Can 3");
-            break;
-		    }
-		if (vote[2] == Epin[2])
-            {
-            line_disp("Send1 Withdraw2");
-            break;
-            }		
-  		}
-			i++;
+	        lcdcmd(0xc0); //decimal value: 193
+            display_vote(party1);
+            lcdcmd(0xc4);
+            display_vote(party2);
+            lcdcmd(0xc8);
+            display_vote(party3);
+            lcdcmd(0xcc);
+            display_vote(party4);
         }
-		
+    thank_func(); 
+    j++;
+    i++;
     }
+    return 0;
+}
+
+
+void display_vote(unsigned int party)  // send 0-9 character values
+{ 
+int k,p;
+for (k=0;k<=2;k++)
+{
+  vote_no[k] = party % 10;
+  party = party/10;
+}
+for (p=2;p>=0;p--)
+{
+lcddat(vote_no[p] + 48);
+}
+}
+
+
+// Countintg votes function
+void count(){
+    unsigned int m = 0;
+    while (vote[m] != '\0'){
+        Epin[m] = keypad();
+        if (vote[0] == Epin[0]){
+            party1 = party1 + 1;
+            break;
+        }
+        if (vote[1] == Epin[1]){
+            party2 = party2 + 1;
+            break;
+        }
+        if (vote[2] == Epin[2]){
+            party3 = party3 + 1;
+            break;
+        }
+        if (vote[3] == Epin[3]){
+            party4 = party4 + 1;
+            break;
+        }
+    }
+    m++;
+}
+
+// password check function and run the door motor
+int check()
+{
+    //  compare the input value with the assign password value
+    if (pin[0] == Epin[0] && pin[1] == Epin[1] && pin[2] == Epin[2] && pin[3] == Epin[3] && pin[4] == Epin[4])
+    {
+        delay(500);
+        lcdcmd(0x01); //decimal value: 1
+        lcdcmd(0x81); //decimal value: 129
+        // show pin is correct
+        lcddisplay("PIN CORRECT");
+        delay(500);
+        // door motor will run
+        motorpin1 = 1;
+        motorpin2 = 0;
+        lcdcmd(0xc1); //decimal value: 193
+        // show the door is unlocked
+        lcddisplay("AUTHENTICATED");
+        delay(10000);
+        motorpin1 = 1;
+        motorpin2 = 0;
+        lcdcmd(0x01); //decimal value: 1
+		return 1;
+    }
+    else
+    {
+        lcdcmd(0x01); //decimal value: 1
+        lcdcmd(0x80); //decimal value: 128
+        lcddisplay("WRONG PIN");
+        delay(500);
+        lcdcmd(0x01); //decimal value: 1
+		return 0;
+    }
+}
+
+
+void line_disp(unsigned char *c)
+{
+		
+		lcdcmd(0x01); //decimal value: 1
+	    lcdcmd(0x80); //decimal value: 129
+ 	    // show pin is correct
+  	    lcddisplay(c);
+}
+
+// Thank You function
+void thank_func(){
+    lcdcmd(0x01);
+    lcdcmd(0x80);
+    lcddisplay("Thank You");
+    lcdcmd(0xc0);
+    lcddisplay("Response Collected");
 }
 
 //delay function
@@ -244,53 +352,6 @@ char keypad()
             return '#';
         }
     }
-}
-
-// password check function and run the door motor
-
-int check()
-{
-    //  compare the input value with the assign password value
-    if (pin[0] == Epin[0] && pin[1] == Epin[1] && pin[2] == Epin[2] && pin[3] == Epin[3] && pin[4] == Epin[4])
-    {
-        delay(1000);
-        lcdcmd(0x01); //decimal value: 1
-        lcdcmd(0x81); //decimal value: 129
-        // show pin is correct
-        lcddisplay("PIN CORRECT");
-        delay(1000);
-        // door motor will run
-        motorpin1 = 1;
-        motorpin2 = 0;
-        lcdcmd(0xc1); //decimal value: 193
-        // show the door is unlocked
-        lcddisplay("AUTHENTICATED");
-        delay(10000);
-        motorpin1 = 1;
-        motorpin2 = 0;
-        lcdcmd(0x01); //decimal value: 1
-		return 1;
-    }
-    else
-    {
-        lcdcmd(0x01); //decimal value: 1
-        lcdcmd(0x80); //decimal value: 128
-        lcddisplay("WRONG PIN");
-        delay(1000);
-        lcdcmd(0x01); //decimal value: 1
-		return 0;
-    }
-}
-void line_disp(unsigned char *c)
-{
-		
-		lcdcmd(0x01); //decimal value: 1
-	    lcdcmd(0x81); //decimal value: 129
- 	    // show pin is correct
-  	    lcddisplay(c);
-	    delay(1000);
-	    lcdcmd(0xc1); //decimal value: 193
-   	    // show the door is unlocked
 }
 
 end;
